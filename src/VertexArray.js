2f94392d31848dc7a25319906ba399491bd89463
frezmecritus <frezmecritus@gmail.com>
2017-01-23 00:20:30 -0800

    First Commit

---
+++ src/VertexArray.js
@@ -0,0 +1,31 @@
+var VertexArray = function(length){
+  array = new Float32Array(length);
+  objectsNameStartPositionMap = {};
+  currentEnd = 0;
+
+  this.getArray = function () {
+    return array;
+  }
+
+  this.appendObject = function (objectName, vertices) {
+    objectsNameStartPositionMap[objectName] = currentEnd;
+    appendVertices(vertices);
+    currentEnd += vertices.length;
+  }
+
+  function appendVertices(vertices) {
+    try {
+        array.set(vertices, currentEnd);
+    } catch (e) {
+        throw e;
+    }
+  }
+
+  this.getObjectStartPosition = function(objectName) {
+    return objectsNameStartPositionMap[objectName];
+  }
+}
+
+if(typeof exports !== 'undefined') {
+    module.exports = VertexArray;
+}
